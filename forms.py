43ac66eb9c3b290d8abab97d668cce138f2ccfe1
Kiru <kirushanth.ravindran@gmail.com>
2021-04-30 16:21:07 -0700

    finished part 1

--- forms.py
+++ forms.py
@@ -1,15 +1,18 @@
 """Forms for adopt app."""
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField, BooleanField, IntegerField, SelectField, 
+from wtforms import StringField, FloatField, BooleanField, IntegerField, SelectField 
 from wtforms.validators import InputRequired, Optional, URL
 
 class NewPetForm(FlaskForm):
-"""Form for adding new pet"""
+    """Form for adding new pet"""
     name = StringField("Name",
                        validators=[InputRequired()])
     
-    species = StringField("Species",
-                       validators=[InputRequired()])
+    species = SelectField("Species",
+                choices=[('dog', 'Dog'),
+                         ('cat', 'Cat'),
+                         ('porcupine', 'Porcupine')],
+                         validators=[InputRequired()])
     
     photo_url = StringField("Photo url",
                        validators=[Optional(), URL()])
@@ -23,7 +26,16 @@ class NewPetForm(FlaskForm):
     
     notes = StringField("Notes",
                        validators=[Optional()])
+
+class EditPetForm(FlaskForm):
+    """Form for editting a pet"""
+
+    photo_url = StringField("Photo url",
+                       validators=[Optional(), URL()])
     
+    notes = StringField("Notes",
+                       validators=[Optional()])
+
     available = BooleanField("Available",
-                       validators=[InputRequired()])
+                       validators=[Optional()])
    
--- seed.py
+++ seed.py
@@ -12,14 +12,16 @@ Pet.query.delete()
 
 
 # Add users
-vin = Pet(name="vin", species="Dog", 
+vin = Pet(name="vin", species="dog", 
 photo_url="https://media1.popsugar-assets.com/files/thumbor/Xqi0W8lbQkX_boK4ZNCAUsLQ6cg/220x77:2725x2582/fit-in/500x500/filters:format_auto-!!-:strip_icc-!!-/2020/06/30/762/n/1922398/abb19e095efb73c3ee1500.23266776_/i/Vin-Diesel.jpg",
 age="young")
-
+diesal = Pet(name="Diesal", species="cat", 
+photo_url="https://media1.popsugar-assets.com/files/thumbor/Xqi0W8lbQkX_boK4ZNCAUsLQ6cg/220x77:2725x2582/fit-in/500x500/filters:format_auto-!!-:strip_icc-!!-/2020/06/30/762/n/1922398/abb19e095efb73c3ee1500.23266776_/i/Vin-Diesel.jpg",
+age="old")
 
 # Add new objects to session, so they'll persist
 db.session.add(vin)
-
+db.session.add(diesal)
 
 # add users to temp DB with flush so we can add some sample posts
 db.session.commit()
