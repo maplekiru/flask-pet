0e6d16fa9b67424e37bcb7a1d30f07de4c5c8590
Perryvon <Perry.j.vr@gmail.com>
2021-04-30 12:32:22 -0700

    added first form for submit new pet and made html template for it

--- forms.py
+++ forms.py
@@ -1,1 +1,29 @@
 """Forms for adopt app."""
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, BooleanField, IntegerField, SelectField, 
+from wtforms.validators import InputRequired, Optional, URL
+
+class NewPetForm(FlaskForm):
+"""Form for adding new pet"""
+    name = StringField("Name",
+                       validators=[InputRequired()])
+    
+    species = StringField("Species",
+                       validators=[InputRequired()])
+    
+    photo_url = StringField("Photo url",
+                       validators=[Optional(), URL()])
+    
+    age = SelectField('Age',
+                choices=[('baby', 'Baby'),
+                         ('young', 'Young'),
+                         ('adult', 'Adult'),
+                         ('senior', 'Senior')],
+                         validators=[InputRequired()])
+    
+    notes = StringField("Notes",
+                       validators=[Optional()])
+    
+    available = BooleanField("Available",
+                       validators=[InputRequired()])
+   
